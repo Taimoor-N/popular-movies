184bbc10c26bc6486baa89b3e300c0eab59e3c51
Taimoor Najam <taimoor_najam@yahoo.com>
2019-07-14 17:44:38 -0400

    Created RecyclerView with Grid Layout, created adapter for RecyclerView and added sample data

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] sampleImages =
+                  {"https://picsum.photos/id/10/768/1366",
+                   "https://picsum.photos/id/20/768/1366",
+                   "https://picsum.photos/id/30/768/1366",
+                   "https://picsum.photos/id/40/768/1366",
+                   "https://picsum.photos/id/50/768/1366",
+                   "https://picsum.photos/id/60/768/1366",
+                   "https://picsum.photos/id/70/768/1366",
+                   "https://picsum.photos/id/80/768/1366",
+                   "https://picsum.photos/id/90/768/1366",
+                   "https://picsum.photos/id/100/768/1366",
+                   "https://picsum.photos/id/110/768/1366",
+                   "https://picsum.photos/id/120/768/1366",
+                   "https://picsum.photos/id/130/768/1366",
+                   "https://picsum.photos/id/140/768/1366",
+                   "https://picsum.photos/id/150/768/1366",
+                   "https://picsum.photos/id/160/768/1366",
+                   "https://picsum.photos/id/170/768/1366",
+                   "https://picsum.photos/id/180/768/1366",
+                   "https://picsum.photos/id/190/768/1366",
+                   "https://picsum.photos/id/200/768/1366"};
+
+        mRecyclerView = findViewById(R.id.rv_movies);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setAdapter(new MovieAdapter(sampleImages));
     }
 }
